c5901bd79190264d96d2537f1d26f6a531e5b7ca
Oceane <kenfacktonfack.o@northeastern.edu>
2024-08-19 09:22:37 -0400

    Fixed Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Labs from "./Labs";
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Kanbas from "./Kanbas"
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <p>Oceane Kenfack Tonfack, Section 61065</p>
       <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
