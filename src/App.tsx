0cbb609eedc13b6367bf041b6cae45bf380a7fb2
Oceane <kenfacktonfack.o@northeastern.edu>
2024-08-19 09:17:23 -0400

    landing page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 
 import './App.css';
 import Labs from "./Labs";
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Kanbas from "./Kanbas"
 
 function App() {
   return (
     <HashRouter>
       <div className="App">
+        <p>Oceane Kenfack Tonfack, Section 61065</p>
       <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
